445acdd59d6326ae64b694eb652dee81e325faad
ElijahBerkley-pioneerhigh <322073@aaps.k12.mi.us>
2024-09-09 09:28:15 -0400

    Addef unnier copilot

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Why did the chicken cross the road? To say 'Hello World!'");
+        System.out.println("Why did the chicken cross the road? To say 'Hello World!' and avoid the Java NullPointerException!");
+        System.out.println("But then it realized that NullPointerExceptions can be quite 'chicken' too!");
+        System.out.println("So it decided to embrace the NullPointerException and join the Java party!");
+        System.out.println("And that's how the chicken became the life of the Java party!");
+        System.out.println("And it danced the night away, throwing NullPointerExceptions left and right!");
+        System.out.println("Everyone at the Java party couldn't help but laugh and join in on the fun!");
+        System.out.println("The chicken became a Java legend, known for its fearless embrace of NullPointerExceptions!");
+        System.out.println("And that's the story of how the chicken became the ultimate Java comedian!");
+        System.out.println("But wait, there's more!");
+        System.out.println("The chicken went on to write a best-selling book called 'Null Pointers and Chicken Feathers: A Java Comedy'");
+        System.out.println("It also started a successful stand-up comedy tour, performing at Java conferences worldwide!");
+        System.out.println("The chicken's catchphrase became 'NullPointerExceptions are clucking hilarious!'");
+        System.out.println("And it continued to spread laughter and joy throughout the Java community!");
     }
 }
